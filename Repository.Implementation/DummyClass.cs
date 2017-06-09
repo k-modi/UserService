a97e076b169d61d1e0c7b53ca289ba2bdf9b20ea
Kunjan Modi Personal <kunjan.modi@gmail.com>
2017-06-09 00:13:20 -0700

    undo change t5

--- Repository.Implementation/DummyClass.cs
+++ Repository.Implementation/DummyClass.cs
@@ -3,10 +3,8 @@ namespace Repository.Implementation
 {
     public class DummyClass
     {
-        private string _something;
         public DummyClass(string something)
         {
-            _something = something;
         }
     }
 }
